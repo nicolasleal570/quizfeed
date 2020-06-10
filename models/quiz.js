32554e7eb675831b1e7b220156bbf08dd41bc45e
Nicolas Leal <lealnicolas96@gmail.com>
2020-06-09 23:51:12 -0400

    Update quiz.js pt. 2
    they made a typing mistake in the quiz model. author_id field and title

--- models/quiz.js
+++ models/quiz.js
@@ -4,8 +4,8 @@ const Schema   = mongoose.Schema;
 const quizSchema = new Schema({
     
     _id: {type: String, required: true},
-    title:        {type: String, requried: true},
-    author_id:   {type: String, requried: true},
+    title:        {type: String, required: true},
+    author_id:   {type: String, required: true},
     last_edited: { type: Date, default: Date.now }
 
 },{ strict: false });
